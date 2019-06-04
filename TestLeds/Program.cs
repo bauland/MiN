41b5663c54aae62c33774054a9255580d8dcf464
Bauland <nicolas.bauland@laposte.net>
2019-06-04 05:58:03 +0200

    Project to test

--- TestLeds/Program.cs
+++ TestLeds/Program.cs
@@ -1,14 +1,31 @@
-﻿using System;
-using System.Collections;
-using System.Text;
-using System.Threading;
+﻿using System.Threading;
+using Bauland.Others;
+using GHIElectronics.TinyCLR.Pins;
+// ReSharper disable FunctionNeverReturns
 
 namespace TestLeds
 {
-    class Program
+    static class Program
     {
+        private static LedStrip _ledStrip;
         static void Main()
         {
+            _ledStrip=new LedStrip(8,BrainPad.Expansion.SpiBus.Spi1,LedStrip.ColorOrder.Bgr);
+            _ledStrip.Clear();
+            _ledStrip.Show();
+
+            while (true)
+            {
+                _ledStrip.SetAll(LedStrip.IntensityMax, 255, 255, 255);
+                _ledStrip.Show();
+                Thread.Sleep(2000);
+                _ledStrip.SetAll(LedStrip.IntensityMax, 0, 0, 255);
+                _ledStrip.Show();
+                Thread.Sleep(2000);
+                _ledStrip.Clear();
+                _ledStrip.Show();
+                Thread.Sleep(2000);
+            }
         }
     }
 }
